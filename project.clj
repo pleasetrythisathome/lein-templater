75e0b80295caeb0dd746b0f09b3e88daf64f174c
Dylan Butman <dbutman@nyu.edu>
2014-08-18 14:13:43 -0400

    deps

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject lein-templater "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A leiningen plugin for creating leiningen templates "
+  :url "https://github.com/pleasetrythisathome/lein-templater"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[slothcfg "1.0.1"]
+                 [rewrite-clj "0.3.9"]]
   :eval-in-leiningen true)
