612339cfdc29a5f3fa30ced4cb6c0b6edccad198
Dylan Butman <dbutman@nyu.edu>
2014-08-19 11:58:21 -0400

    deps

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/pleasetrythisathome/lein-templater"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[slothcfg "1.0.1"]
+  :dependencies [[org.clojure/core.match "0.2.1"]
+                 [slothcfg "1.0.1"]
                  [rewrite-clj "0.3.9"]]
   :eval-in-leiningen true)
